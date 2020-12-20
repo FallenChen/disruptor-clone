286f5f9b7c0d08061fd3156c948355da94f1b3de
GarryChen <chenymj23@gmail.com>
2020-12-20 22:37:09 +0800

    WaitStrategy Api and BlockingStrategy init

--- disruptor-clone/src/main/java/org/garry/disruptor_clone/WaitStrategy.java
+++ disruptor-clone/src/main/java/org/garry/disruptor_clone/WaitStrategy.java
@@ -1,5 +1,10 @@
 package org.garry.disruptor_clone;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Strategy employed for making {@link Consumer}s wait on a {@link RingBuffer}
  */
@@ -7,11 +12,46 @@ public interface WaitStrategy {
 
     /**
      * Wait for the given sequence to be available for consumption in a {@link RingBuffer}
-     * @param consumers
+     * @param consumers future
      * @param ringBuffer
      * @param barrier
      * @param sequence
      * @return
      */
     long waitFor(Consumer[] consumers, RingBuffer ringBuffer,ConsumerBarrier barrier, long sequence);
+
+
+    long waitFor(Consumer[] consumers, RingBuffer ringBuffer, ConsumerBarrier barrier, long sequence, long timeout, TimeUnit units);
+
+
+    void signalAll();
+
+
+    /**
+     * Blocking strategy that uses a lock and condition variable for {@link Consumer}s waiting on a barrier
+     *
+     * This strategy should be used when performances and low-latency are not as important as CPU resource
+     */
+    static final class BlockingStrategy implements WaitStrategy
+    {
+        private final Lock lock = new ReentrantLock();
+        private final Condition consumerNotifyCondition = lock.newCondition();
+
+        @Override
+        public long waitFor(Consumer[] consumers, RingBuffer ringBuffer, ConsumerBarrier barrier, long sequence) {
+            return 0;
+        }
+
+        @Override
+        public long waitFor(Consumer[] consumers, RingBuffer ringBuffer, ConsumerBarrier barrier, long sequence, long timeout, TimeUnit units) {
+            return 0;
+        }
+
+        @Override
+        public void signalAll() {
+
+        }
+    }
+
+
 }
